284b285373fc048bcbb95d7cc073bc35204cf6b8
RahulSaini-172 <rahulsaini8362@gmail.com>
2025-06-26 11:28:08 +0000

    data types

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,23 @@
 console.log("Rahul")
+"use strict"; // treat all js code as newer version 
+//alert(3+3) //we are using node js not browser 
+
+let name ="rahul";
+let age = 22
+let isloggedIn = false
+let state;
+
+//number = 2 to power 53
+// bigint
+//string 
+//boolean =true/false
+//null =standalone value
+//undefined =
+//symbol=unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
